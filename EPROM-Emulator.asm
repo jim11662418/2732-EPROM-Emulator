aecfb2c357983d9fb5f0259bd6e3e6e20dd5d808
Jim <51113579+jim11662418@users.noreply.github.com>
2020-03-11 07:34:51 -0400

    Update EPROM-Emulator.asm

--- EPROM-Emulator.asm
+++ EPROM-Emulator.asm
@@ -1,3 +1,19 @@
+;---------------------------------------------------------------------------------------------------------------------------------
+; Copyright 2020 Jim Loos
+; 
+; Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+; (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+; publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+; so, subject to the following conditions:
+; 
+; The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+; 
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+; IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;---------------------------------------------------------------------------------------------------------------------------------
+
 ; Load a Intel hex file into IDT7134 Dual-port RAM.
 ; the Dual-port RAM connects through a ribbon cable 
 ; to the 2732 EPROM socket on the 4004 single
@@ -15,8 +31,8 @@ esc             .equ    1BH
 
 LED             .equ    T0              ;LED connected to T0
 
-bps28800	    .equ    0FFH
-bps14400	    .equ    0FEH
+bps28800	      .equ    0FFH
+bps14400	      .equ    0FEH
 bps9600	        .equ    0FDH
 bps4800	        .equ    0FAH
 bps2400	        .equ    0F4H
